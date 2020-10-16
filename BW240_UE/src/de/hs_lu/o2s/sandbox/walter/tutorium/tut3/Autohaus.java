bf19774d6090103343ca6e8c79bf93c7916e0899
Christian Walter <itscLx@users.noreply.github.com>
2020-10-16 10:23:36 +0200

    autohaus

--- BW240_UE/src/de/hs_lu/o2s/sandbox/walter/tutorium/tut3/Autohaus.java
+++ BW240_UE/src/de/hs_lu/o2s/sandbox/walter/tutorium/tut3/Autohaus.java
@@ -1,7 +1,20 @@
 package de.hs_lu.o2s.sandbox.walter.tutorium.tut3;
 
+import de.hs_lu.o2s.sandbox.walter.ueb.ue1.Person;
+
+import java.util.Arrays;
+import java.util.List;
+
+import de.hs_lu.o2s.sandbox.walter.tutorium.tut2.*;
+
+
 public class Autohaus {
 	String name;
+	private Auto[] autolist;
+	private Person[] personenlist;
+	private List<Haus> gebäude;
+
+	
 	
 	
 }
